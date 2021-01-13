7b8a7458a0d99733cd5cc92f8e9d5ae50dd4d008
mveynberg <mv@cj264.ru>
2021-01-14 01:41:03 +0500

    Step 4

--- src/main/java/ru/cj264/geekbrains/java_core_for_android/hw3/TicTacToe.java
+++ src/main/java/ru/cj264/geekbrains/java_core_for_android/hw3/TicTacToe.java
@@ -1,5 +1,6 @@
 package ru.cj264.geekbrains.java_core_for_android.hw3;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,9 +13,11 @@ public class TicTacToe {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final Random rand = new Random();
 
-    private static final int fieldSizeX = 5;
-    private static final int fieldSizeY = 5;
+    private static final int fieldSizeX = 3;
+    private static final int fieldSizeY = 3;
     private static final int lineLength = 3;
+
+    private static long aiTimeFrom;
 
     private static void initField() {
         field = new char[fieldSizeY][fieldSizeX];
@@ -27,9 +30,38 @@ public class TicTacToe {
     }
 
     private static void printField() {
+        printField(field);
+    }
+
+    private static void printField(char[][] field) {
+        System.out.print("   ");
+        for (int i = 1; i <= fieldSizeX; i++) {
+            System.out.printf("%3d", i);
+        }
+        System.out.println();
+        int row = 1;
         for (char[] j : field) {
+            System.out.printf("%3d", row++);
             for (char i : j) {
-                System.out.print(i + "  ");
+                System.out.printf("%3c", i);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private static void printSteps(int[][] steps) {
+        System.out.print("   ");
+        for (int i = 1; i <= fieldSizeX; i++) {
+            System.out.printf("%3d", i);
+        }
+        System.out.println();
+
+        int row = 1;
+        for (int[] j : steps) {
+            System.out.printf("%3d", row++);
+            for (int i : j) {
+                System.out.printf("%3d", i);
             }
             System.out.println();
         }
@@ -48,18 +80,148 @@ public class TicTacToe {
         field[y][x] = DOT_X;
     }
 
+    private static char[][] deepCopy(char[][] matrix) {
+        return Arrays.stream(matrix).map(el -> el.clone()).toArray($ -> matrix.clone());
+    }
+
+    private static int[][] deepCopy(int[][] matrix) {
+        return Arrays.stream(matrix).map(el -> el.clone()).toArray($ -> matrix.clone());
+    }
+
+    private static int aiCheckTurn(int x, int y, char aiDot, char[][] field, int[][] steps, int inc) {
+        field[y][x] = aiDot;
+        steps[y][x] = inc;
+
+        if (nInLine(field)) {
+            return 1;
+        } else if (checkDraw(field)) {
+            return 2;
+        } else {
+            char opponentDot = aiDot == DOT_O ? DOT_X : DOT_O;
+            int[] opponentTurnXY = aiCalculateTurn(opponentDot, deepCopy(field), deepCopy(steps), inc + 1);
+            field[opponentTurnXY[0]][opponentTurnXY[1]] = opponentDot;
+
+            if (nInLine(field)) {
+                return fieldSizeX*fieldSizeY+1;
+            } else if (checkDraw(field)) {
+                return 2;
+            } else {
+                int[] aiTurnXY = aiCalculateTurn(aiDot, deepCopy(field), deepCopy(steps), inc + 2);
+                if (aiTurnXY[0] == -1 || field[aiTurnXY[0]][aiTurnXY[1]] != DOT_EMPTY) {
+                    System.out.println("!!!");
+                }
+                return aiCheckTurn(aiTurnXY[1], aiTurnXY[0], aiDot, deepCopy(field), deepCopy(steps), inc + 2) + 1;
+            }
+        }
+    }
+
+    private static void drawWind(int x, int y, char[][] field, int[][] calcField) {
+        int[][] directions = {{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+        int windX; int windY;
+        int forceWind;
+        char opponentDot = field[y][x];
+        char myDot = opponentDot == DOT_X ? DOT_O : DOT_X;
+        for (int j = 0; j < 8; j++) {
+            windX = x;
+            windY = y;
+            forceWind = 0;
+            windDirection:
+            for (int i = 1; i < lineLength; i++) {
+                do {
+                    windX += directions[j][1];
+                    windY += directions[j][0];
+                    if (isCellValid(windX, windY) && field[windY][windX] == myDot) break windDirection;
+                    if (isCellValid(windX, windY) && field[windY][windX] == opponentDot) forceWind += 1;
+                } while (isCellValid(windX, windY) && !isCellEmpty(windX, windY, field));
+                if (isCellValid(windX, windY)) {
+                    calcField[windY][windX] += lineLength - i + forceWind;
+                    forceWind = 0;
+                } else break windDirection;
+            }
+        }
+    }
+
+    private static int[] aiPreventLine(char aiDot, char[][] field, int inc) {
+        char opponentDot = aiDot == DOT_X ? DOT_O : DOT_X;
+
+        int maxX = -1; int maxY = -1; int maxCalc = 0;
+
+        int[][] calcField = new int[fieldSizeY][fieldSizeX];
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (field[y][x] == opponentDot) {
+                    drawWind(x, y, field, calcField);
+                }
+            }
+        }
+
+//        // DEBUG
+//        if (inc == 1) {
+//            System.out.println("Prevent Line");
+//            printSteps(calcField);
+//        }
+
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (calcField[y][x] > maxCalc) {
+                    maxCalc = calcField[y][x];
+                    maxX = x;
+                    maxY = y;
+                }
+            }
+        }
+
+        return new int[]{maxY, maxX};
+    }
+
+    private static int[] aiCalculateTurn(char aiDot, char[][] field, int[][] steps, int inc) {
+        int aiX = -1; int aiY = -1;
+        int turnsCount;
+        int aiFastest = fieldSizeX*fieldSizeY*2;
+        calculate:
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (System.nanoTime() - aiTimeFrom > 5_000_000_000L) {
+                    aiX = -1;
+                    aiY = -1;
+                    break calculate;
+                }
+                if (field[y][x] == DOT_EMPTY) {
+                    turnsCount = aiCheckTurn(x, y, aiDot, deepCopy(field), deepCopy(steps), inc);
+                    if (turnsCount < aiFastest) {
+                        aiY = y;
+                        aiX = x;
+                        aiFastest = turnsCount;
+                        if (aiFastest == 1) break calculate;
+                    }
+                }
+            }
+        }
+        if (aiY == -1) {
+            int[] preventXY = aiPreventLine(aiDot, field, inc);
+            aiY = preventXY[0];
+            aiX = preventXY[1];
+        }
+        if (aiY == -1) {
+            do {
+                aiX = rand.nextInt(fieldSizeX);
+                aiY = rand.nextInt(fieldSizeY);
+            } while (!isCellEmpty(aiX, aiY, field));
+        }
+        return new int[] {aiY, aiX};
+    }
+
     private static void aiTurn() {
-        int x; int y;
-
-        do {
-            x = rand.nextInt(3);
-            y = rand.nextInt(3);
-        } while (!isCellEmpty(x, y) || !isCellValid(x, y));
-
-        field[y][x] = DOT_O;
+        aiTimeFrom = System.nanoTime();
+        int[] aiTurnXY = aiCalculateTurn(DOT_O, deepCopy(field), new int[fieldSizeY][fieldSizeX], 1);
+        field[aiTurnXY[0]][aiTurnXY[1]] = DOT_O;
     }
 
     private static boolean isCellEmpty(int x, int y) {
+        return isCellEmpty(x, y, field);
+    }
+
+    private static boolean isCellEmpty(int x, int y, char[][] field) {
         return field[y][x] == DOT_EMPTY;
     }
 
@@ -68,7 +230,7 @@ public class TicTacToe {
     }
 
     private static boolean checkGame(String s) {
-        if (nInLine(lineLength)) {
+        if (nInLine()) {
             System.out.println(s);
             return true;
         }
@@ -79,9 +241,13 @@ public class TicTacToe {
         return false;
     }
 
-    private static boolean nInLine(int n) {
-        int x; int y; int i;
+    private static boolean nInLine() {
+        return nInLine(field);
+    }
 
+    private static boolean nInLine(char[][] field) {
+        int x; int y; int i;
+        int n = lineLength;
         char dot;
 
         for (y = 0; y < fieldSizeY; y++) {
@@ -129,9 +295,13 @@ public class TicTacToe {
     }
 
     private static boolean checkDraw() {
+        return checkDraw(field);
+    }
+
+    private static boolean checkDraw(char[][] field) {
         for (int y = 0; y < fieldSizeY; y++) {
             for (int x = 0; x < fieldSizeX; x++) {
-                if (isCellEmpty(x, y)) return false;
+                if (isCellEmpty(x, y, field)) return false;
             }
         }
         return true;
