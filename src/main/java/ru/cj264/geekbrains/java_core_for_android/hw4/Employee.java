7d3e741d27cd5af82d36e5e5d51ddeb163ddda51
mveynberg <mv@cj264.ru>
2021-01-20 15:56:47 +0500

    Step 1

---
+++ src/main/java/ru/cj264/geekbrains/java_core_for_android/hw4/Employee.java
@@ -0,0 +1,13 @@
+package ru.cj264.geekbrains.java_core_for_android.hw4;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private float salary;
+    private int age;
+
+}
