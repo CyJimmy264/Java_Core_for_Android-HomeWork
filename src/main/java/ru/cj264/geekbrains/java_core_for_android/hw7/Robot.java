d01505b3c12b6170d903ab2c920235796befce07
mveynberg <mv@cj264.ru>
2021-01-26 23:21:48 +0500

    Refactor step 1

--- src/main/java/ru/cj264/geekbrains/java_core_for_android/hw7/Robot.java
+++ src/main/java/ru/cj264/geekbrains/java_core_for_android/hw7/Robot.java
@@ -3,17 +3,8 @@ package ru.cj264.geekbrains.java_core_for_android.hw7;
 public class Robot implements Running {
     String id;
 
-    public Robot(String id) {
-        this.id = id;
-    }
+    public Robot(String id) { this.id = id; }
 
     @Override
-    public void run() {
-        System.out.println("Robot " + id + " runs.");
-    }
-
-    @Override
-    public void jump() {
-        System.out.println("Robot " + id + " jumps.");
-    }
+    public String getName() { return "Robot " + id; }
 }
--- src/main/java/ru/cj264/geekbrains/java_core_for_android/hw7/Running.java
+++ src/main/java/ru/cj264/geekbrains/java_core_for_android/hw7/Running.java
@@ -1,6 +1,13 @@
 package ru.cj264.geekbrains.java_core_for_android.hw7;
 
 public interface Running {
-    void run();
-    void jump();
+    String getName();
+
+    default void run() {
+        System.out.println(getName() + " runs.");
+    }
+
+    default void jump() {
+        System.out.println(getName() + " jumps.");
+    }
 }
