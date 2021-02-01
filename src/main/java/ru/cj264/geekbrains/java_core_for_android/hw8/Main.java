516a015c73393cff21b76e79883260075bf20475
mveynberg <mv@cj264.ru>
2021-02-01 12:56:30 +0500

    Step 1

--- src/main/java/ru/cj264/geekbrains/java_core_for_android/hw8/Main.java
+++ src/main/java/ru/cj264/geekbrains/java_core_for_android/hw8/Main.java
@@ -1,8 +1,31 @@
 package ru.cj264.geekbrains.java_core_for_android.hw8;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
+    public static void createTextFile(String filename, String text) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename)))
+        {
+            writer.write(text);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         // 1. Создать 2 текстовых файла, примерно по 50-100 символов в каждом(особого значения не имеет);
+        createTextFile("sample1.txt",
+                "1. Создать 2 текстовых файла, примерно по 50-100\n"+
+                        " символов в каждом(особого значения не имеет);\n");
+        createTextFile("sample2.txt",
+                "2. Написать метод, «склеивающий» эти файлы, то есть вначале идет текст из первого файла,\n" +
+                        " потом текст из второго.\n");
+
         // 2. Написать метод, «склеивающий» эти файлы, то есть вначале идет текст из первого файла,
         //    потом текст из второго.
         // 3. * Написать метод, который проверяет присутствует ли указанное пользователем слово в файле
