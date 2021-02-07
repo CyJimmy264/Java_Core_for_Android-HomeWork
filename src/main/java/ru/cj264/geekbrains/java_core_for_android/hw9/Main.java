6a0ce0c45bf5ad6f6939e5efafe8df2da3119963
mveynberg <mv@cj264.ru>
2021-02-07 18:45:15 +0500

    Initial commit

---
+++ src/main/java/ru/cj264/geekbrains/java_core_for_android/hw9/Main.java
@@ -0,0 +1,12 @@
+package ru.cj264.geekbrains.java_core_for_android.hw9;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Создать класс кота
+        // 2. Создать в бд таблицу с котами
+        // 3. Написать метод извлечения котов
+        // 4. Метод добавления котов
+        // 5. Метод удаления котов
+        // 6. Метод изменения котов
+    }
+}
