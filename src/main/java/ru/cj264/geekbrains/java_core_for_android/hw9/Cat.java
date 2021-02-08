429b7ab4189e323f9d39564d2f3e76bf88461020
mveynberg <mv@cj264.ru>
2021-02-08 16:17:27 +0500

    Steps 2, 4

--- src/main/java/ru/cj264/geekbrains/java_core_for_android/hw9/Cat.java
+++ src/main/java/ru/cj264/geekbrains/java_core_for_android/hw9/Cat.java
@@ -1,8 +1,12 @@
 package ru.cj264.geekbrains.java_core_for_android.hw9;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 
 public class Cat {
+    long id;
     String name;
     LocalDate birthday;
     String color;
@@ -23,5 +27,28 @@ public class Cat {
                 ", color='" + color + '\'' +
                 ", breed='" + breed + '\'' +
                 '}';
+    }
+
+    public long create(Statement statement) throws SQLException {
+        int affectedRows = statement.executeUpdate(
+                String.format("INSERT INTO `Cats` (`Name`, `Birthday`, `Color`, `Breed`) "+
+                                "VALUES ('%s', '%s', '%s', '%s');",
+                        name, birthday, color, breed)
+        );
+
+        if (affectedRows == 0) {
+            throw new SQLException("Creating user failed, no rows affected.");
+        }
+
+        ResultSet generatedKeys = statement.getGeneratedKeys();
+
+        if (generatedKeys.next()) {
+            id = generatedKeys.getLong(1);
+        }
+        else {
+            throw new SQLException("Creating user failed, no ID obtained.");
+        }
+
+        return id;
     }
 }
--- src/main/java/ru/cj264/geekbrains/java_core_for_android/hw9/Main.java
+++ src/main/java/ru/cj264/geekbrains/java_core_for_android/hw9/Main.java
@@ -1,19 +1,64 @@
 package ru.cj264.geekbrains.java_core_for_android.hw9;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
-import java.util.Date;
 
 public class Main {
+    private static Connection connection;
+    private static Statement statement;
+
+    private static String dropCats = "DROP TABLE IF EXISTS `Cats`;";
+    private static String primaryLoad =
+        "CREATE TABLE IF NOT EXISTS `Cats` (" +
+        "  `CatID` INTEGER NOT NULL," +
+        "  `Name` TEXT NOT NULL," +
+        "  `Birthday` DATE NOT NULL," +
+        "  `Color` TEXT NOT NULL," +
+        "  `Breed` TEXT NOT NULL," +
+        "  PRIMARY KEY(`CatID` AUTOINCREMENT)" +
+        ");";
+
+    private static void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:cats.db");
+    }
+
+    private static void close() throws SQLException {
+        connection.close();
+    }
+
+    private static void database() {
+        try {
+            connect();
+
+            statement = connection.createStatement();
+            statement.execute(dropCats);
+            statement.execute(primaryLoad);
+
+            Cat cat = new Cat("Barsik", LocalDate.of(2007, 11, 13),
+                    "white and black", "siamese");
+
+            long catId = cat.create(statement);
+            System.out.println("Cat record with id " + catId + " created");
+
+            close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         // 1. Создать класс кота
+
         // 2. Создать в бд таблицу с котами
-        // 3. Написать метод извлечения котов
         // 4. Метод добавления котов
+
+        // 3. Написать метод извлечения котов
         // 5. Метод удаления котов
         // 6. Метод изменения котов
-
-        Cat cat = new Cat("Barsik", LocalDate.of(2007, 11, 13),
-                "white and black", "siamese");
-        System.out.println(cat);
+        database();
     }
 }
